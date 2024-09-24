ab13671d7a371877d558f324345daf788e797848
csf23mcr2 <csf23mcr2@MF124PC78>
2024-09-24 15:50:33 +0100

    Add project files.

---
+++ Week 1 Practical/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Cache;
+
+//Console.WriteLine("Please enter your name");
+
+//string name = Console.ReadLine();
+
+//Console.WriteLine("Please enter your age");
+
+//int Age = Convert.ToInt32(Console.ReadLine());
+
+//int result = Age + 5;
+
+//Console.WriteLine("Hello {0} you will be {1} in 5 years", name,result, );
+
+//Console.WriteLine("Please enter your age");
+
+//int Age = Convert.ToInt32(Console.ReadLine());
+
+//if (Age > 0 && Age < 13)
+//{
+// Console.WriteLine("Child");
+//}
+
+//else if (Age > 12 && Age < 20)
+//{
+//Console.WriteLine("Teenager");
+//}
+
+//else if ( Age > 19 && Age < 111)
+//  }
+//Console.WriteLine("Adult");
+//}
+
+//else if (Age > 110)
+//{
+//Console.WriteLine("Invalid Age");
+//}
+
+//else if (Age < 0)
+//{
+// Console.WriteLine("Invalid Age");
+//}
+
+
+//Console.WriteLine("Please enter an integer");
+
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//if (number > 0)
+//{
+//Console.WriteLine("This is a positive number");
+//}
+
+//else if (number < 0) 
+//{
+//Console.WriteLine("This is a negative number");
+//}
+//Console.WriteLine("Please enter a year");
+//int Year = Convert.ToInt32(Console.ReadLine());
+//int divisor = 4;
+//int remainder = Year % divisor;
+//if (remainder == 0)
+//{
+//    Console.WriteLine("This year is a leap year");
+//}
+//else if (remainder != 0)
+//{
+   //Console.WriteLine("This year is not a leap year");
+//}
+
+
+Console.WriteLine("Please enter an integer");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if(number % 2 == 0)
+        {
+    Console.WriteLine($"{number} is an even number.");
+}
+        else
+{
+    Console.WriteLine($"{number} is an odd number.");
+}
